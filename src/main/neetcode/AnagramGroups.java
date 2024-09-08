43a1c35453e453938f0b869b272ca7460a9069d3
arunmlvtec <arun.pokharna@gmail.com>
2024-09-08 12:33:54 -0400

    Implemented groupAnagrams and added tests

--- src/main/neetcode/AnagramGroups.java
+++ src/main/neetcode/AnagramGroups.java
@@ -1,28 +1,21 @@
 package main.neetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Anagram Groups
  * Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
  * An anagram is a string that contains the exact same characters as another string, but the order of the characters
  * can be different.
- *
  * Example 1:
  * Input: strs = ["act","pots","tops","cat","stop","hat"]
  * Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
- *
  * Example 2:
  * Input: strs = ["x"]
  * Output: [["x"]]
- *
  * Example 3:
  * Input: strs = [""]
  * Output: [[""]]
- *
  * Constraints:
  * 1 <= strs.length <= 1000.
  * 0 <= strs[i].length <= 100
@@ -34,14 +27,14 @@ public class AnagramGroups {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> anagramsMap = new HashMap<>();
         if (strs == null || strs.length == 0) {
-            new ArrayList<>();
+            return new ArrayList<>();
         }
         for (String word: strs) {
             int[] charCount = new int[26];
             for (Character c : word.toCharArray()) {
                 charCount[c - 'a']++;
             }
-            String key = String.valueOf(charCount);
+            String key = Arrays.toString(charCount);
             if (!anagramsMap.containsKey(key)) {
                 anagramsMap.put(key, new ArrayList<>());
             }
--- src/test/neetcode/AnagramGroupsTest.java
+++ src/test/neetcode/AnagramGroupsTest.java
@@ -4,6 +4,8 @@ import main.neetcode.AnagramGroups;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,5 +18,39 @@ class AnagramGroupsTest {
         String[] input = new String[0];
         List<List<String>> expected = new ArrayList<>();
         assertEquals(expected, anagramGroups.groupAnagrams(input));
+    }
+
+    @Test
+    void groupAnagramsTestFilled() {
+        String[] input = {"act","pots","tops","cat","stop","hat"};
+        List<String> l1 = new ArrayList<>();
+        l1.add("hat");
+        List<List<String>> expected = new ArrayList<>(){};
+        expected.add(l1);
+        List<String> l2 = new ArrayList<>();
+        l2.add("act");
+        l2.add("cat");
+        expected.add(l2);
+        List<String> l3 = new ArrayList<>();
+        l3.add("stop");
+        l3.add("pots");
+        l3.add("tops");
+        expected.add(l3);
+        assertTrue(compareLists(expected, anagramGroups.groupAnagrams(input)));
+    }
+
+    private static boolean compareLists(List<List<String>> list1, List<List<String>> list2) {
+        if(list1.size() != list2.size()) {
+            return false;
+        }
+        for (List<String> innerList: list1) {
+            Collections.sort(innerList);
+        }
+        for (List<String> innerList: list2) {
+            Collections.sort(innerList);
+        }
+        list1.sort(Comparator.comparing(Object::toString));
+        list2.sort(Comparator.comparing(Object::toString));
+        return list1.equals(list2);
     }
 }
