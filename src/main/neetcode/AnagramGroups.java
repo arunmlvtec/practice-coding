405608134aa5ada1bb693c295301e6fbd032c4f0
Arun Pokharna <arunmlvtec@users.noreply.github.com>
2024-09-08 12:34:54 -0400

    Anagram groups (#2)
    * AnagramGroups Initial commit
    
    * AnagramGroups Initial commit
    
    * Implemented groupAnagrams and added tests

---
+++ src/main/neetcode/AnagramGroups.java
@@ -0,0 +1,45 @@
+package main.neetcode;
+
+import java.util.*;
+
+/**
+ * Anagram Groups
+ * Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+ * An anagram is a string that contains the exact same characters as another string, but the order of the characters
+ * can be different.
+ * Example 1:
+ * Input: strs = ["act","pots","tops","cat","stop","hat"]
+ * Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+ * Example 2:
+ * Input: strs = ["x"]
+ * Output: [["x"]]
+ * Example 3:
+ * Input: strs = [""]
+ * Output: [[""]]
+ * Constraints:
+ * 1 <= strs.length <= 1000.
+ * 0 <= strs[i].length <= 100
+ * strs[i] is made up of lowercase English letters.
+ *
+ */
+
+public class AnagramGroups {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramsMap = new HashMap<>();
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        for (String word: strs) {
+            int[] charCount = new int[26];
+            for (Character c : word.toCharArray()) {
+                charCount[c - 'a']++;
+            }
+            String key = Arrays.toString(charCount);
+            if (!anagramsMap.containsKey(key)) {
+                anagramsMap.put(key, new ArrayList<>());
+            }
+            anagramsMap.get(key).add(word);
+        }
+        return new ArrayList<>(anagramsMap.values());
+    }
+}
--- src/test/neetcode/IsAnagramTest.java
+++ src/test/neetcode/IsAnagramTest.java
@@ -1,7 +1,7 @@
 package test.neetcode;
 
 import main.neetcode.IsAnagram;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
