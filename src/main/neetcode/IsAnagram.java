306024a219e8e1d12e2db348ba4305135718d7eb
arunmlvtec <arun.pokharna@gmail.com>
2024-09-21 07:20:33 -0400

    refactoring in comments

--- src/main/neetcode/IsAnagram.java
+++ src/main/neetcode/IsAnagram.java
@@ -3,6 +3,21 @@ package main.neetcode;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Is Anagram
+ * Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+ * An anagram is a string that contains the exact same characters as another string, but the order of the characters
+ * can be different.
+ * Example 1:
+ * Input: s = "racecar", t = "carrace"
+ * Output: true
+ * Example 2:
+ * Input: s = "jar", t = "jam"
+ * Output: false
+ * Constraints:
+ * s and t consist of lowercase English letters.
+ * */
+
 public class IsAnagram {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
